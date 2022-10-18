d416e02204e725f2d05640605dcf09a26908254f
Fahad Almusharraf <git@whatmail.org>
2022-10-18 13:03:49 +0300

    add toml for configurations

--- setup.py
+++ setup.py
@@ -3,56 +3,65 @@
 import argparse
 import os
 import platform
-import time
 import shutil
-from pathlib import Path
+import subprocess
+import time
+import tomllib
+import zipfile
 from distutils.spawn import find_executable
-from urllib.request import urlretrieve
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from urllib.request import urlopen
 
 PLATFORM = platform.system()
-DOTFILE_DIR = Path(__file__).parent
-LINKS = {
-    "bin": "~/.bin",
-    "config/Brewfile": "~/.Brewfile",
-    "config/alacritty.yml": "~/.config/alacritty/alacritty.yml",
-    "config/bashrc": "~/.bashrc",
-    "config/config.fish": "~/.config/fish/config.fish",
-    "config/fish_plugins": "~/.config/fish/fish_plugins",
-    "config/gitconfig": "~/.gitconfig",
-    "config/global_ignore": "~/.global_ignore",
-    "config/ideavimrc": "~/.ideavimrc",
-    "config/init.lua": {"Darwin": "~/.hammerspoon/init.lua"},
-    "config/keybindings.json": "~/.vscode/keybindings.json",
-    "config/kitty.conf": "~/.config/kitty/kitty.conf",
-    "config/settings.json": "~/.vscode/settings.json",
-    "config/tmux.conf": "~/.tmux.conf",
-    "config/vimrc": ["~/.vimrc", "~/.config/nvim/init.vim"],
-    "config/zshrc": "~/.zshrc",
-}
-SPOONS = ["Caffeine", "ReloadConfiguration"]
+CONFIGS_TOML = Path(__file__).parent / "config.toml"
+CONFIGS = dict()
+CONFIGS_DIR = CONFIGS_TOML.parent
 
 def setup():
+    global CONFIGS, CONFIGS_DIR
+   
+    with open(CONFIGS_TOML, "rb") as f:
+        CONFIGS = tomllib.load(f)
+   
+    CONFIGS_DIR = CONFIGS["configs_dir"]
+
+    if ARGS.remove_files:
+        remove_files()
+
+    if ARGS.download_files:
+        download_files()
+
     if ARGS.unlink:
-        _unlink()
+        unlink()
 
     if ARGS.link:
-        _link()
+        link()
 
     if ARGS.install:
-        _install()
+        install()
 
-def _symlink_pairs():
-    for src, files in LINKS.items():
-        src = DOTFILE_DIR / src
-        files = files if isinstance(files, list) else [files]
-        for file in files:
-            if isinstance(file, dict):
-                if PLATFORM not in file:
-                    continue
-                file = file[PLATFORM]
-            yield src, Path(file).expanduser()
 
-def _log(msg, header=False, subheader=False, level=0):
+def symlink_pairs():
+    symlinks_map = CONFIGS["symlinks"]
+    symlinks = symlinks_map["common"]
+    if PLATFORM in symlinks_map:
+        symlinks = {**symlinks, **symlinks_map[PLATFORM]}
+
+    for config_dir, filenames in symlinks.items():
+        config_path = CONFIGS_DIR / Path(config_dir)
+        if isinstance(filenames, dict):
+            for source, destination in filenames.items():
+                src = config_path / source
+                dst = Path(os.path.expandvars(destination))
+                yield src, dst
+        elif isinstance(filenames, str):
+            src = config_path
+            dst = Path(os.path.expandvars(filenames))
+            yield src, dst
+
+
+def log(msg, header=False, subheader=False, level=0):
     if header:
         msg = f"--- {msg}".upper()
         width = os.get_terminal_size().columns
@@ -66,18 +75,20 @@ def _log(msg, header=False, subheader=False, level=0):
     else:
         print("  " * level, msg)
 
-def _link():
-    _log(f"linking config files", header=True)
-    for src, dst in _symlink_pairs():
-        _symlink(src, dst)
 
-def _unlink():
-    _log(f"removing files", header=True)
-    for src, dst in _symlink_pairs():
-        _symlink(src, dst, remove=True)
+def link():
+    log(f"linking config files", header=True)
+    for src, dst in symlink_pairs():
+        symlink(src, dst)
 
 
-def _symlink(src_file, dst_file, remove=False):
+def unlink():
+    log(f"removing files", header=True)
+    for src, dst in symlink_pairs():
+        symlink(src, dst, remove=True)
+
+
+def symlink(src_file, dst_file, remove=False):
     msg = f"symlinking: {src_file} --> {dst_file}"
     if remove:
         if dst_file.is_symlink() and not dst_file.exists():
@@ -94,8 +105,8 @@ def _symlink(src_file, dst_file, remove=False):
             if not ARGS.dry_run:
                 shutil.copyfile(src_file, dst_file)
         else:
-          msg += " (IGNORE!! no link or file to deal with)"
-        _log(msg)
+            msg += " (IGNORE!! no link or file to deal with)"
+        log(msg)
     else:
         if dst_file.exists():
             msg += " (EXISTS!!)"
@@ -103,70 +114,92 @@ def _symlink(src_file, dst_file, remove=False):
             if not ARGS.dry_run:
                 dst_file.parent.mkdir(parents=True, exist_ok=True)
                 dst_file.symlink_to(src_file.resolve())
-        _log(msg)
+        log(msg)
 
-def _install():
-    _log(f"installing stuff", header=True)
-    if PLATFORM == "Darwin":
-        _install_darwin()
 
-    vim_plug_url = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-    plug_dirs = [
-        "~/.vim/autoload/plug.vim",
-        "~/.local/share/nvim/site/autoload/plug.vim"
-    ]
-    _log("Installing vim-plug", subheader=True)
-    for plug_dir in plug_dirs:
-        if Path(plug_dir).expanduser().exists():
-            _log(f"{plug_dir} (EXISTS!!)", level=1)
+def remove_files():
+    for download in CONFIGS["downloads"]:
+        url = download["url"]
+        directory = Path(os.path.expandvars(download["directory"]))
+        name = download["name"]
+        target = directory / name
+        if target.exists():
+            log(f"Removing downloaded target {target}")
+            if target.is_dir():
+                if not ARGS.dry_run:
+                    shutil.rmtree(target)
+            else:
+                if not ARGS.dry_run:
+                    target.unlink()
+
+
+def download_files():
+    for download in CONFIGS["downloads"]:
+        url = download["url"]
+        directory = Path(os.path.expandvars(download["directory"]))
+        directory.parent.mkdir(parents=True, exist_ok=True)
+        name = download["name"]
+        log(f"Downloading {url}")
+        if ARGS.dry_run:
+            continue
+        
+        with urlopen(url) as response, NamedTemporaryFile(
+            suffix=".zip", delete=False
+        ) as tmp_fp:
+            shutil.copyfileobj(response, tmp_fp)
+
+        if download["unzip"]:
+            log(f"Unzipping {tmp_fp.name} in {directory}")
+            with zipfile.ZipFile(tmp_fp.name) as zip_fp:
+                zip_fp.extractall(directory)
         else:
-            if not ARGS.dry_run:
-                os.system(f"curl -fLo {plug_dir} --create-dirs {vim_plug_url}")
+            src = Path(tmp_fp.name).rename(name)
+            log(f"Moving {src} to {directory}")
+            shutil.move(src, directory)
 
 
+def install():
+    log(f"installing stuff", header=True)
+    if PLATFORM == "Darwin":
+        install_homebrew()
 
-def _install_darwin():
-    _install_homebrew()
-    _install_hammerspoon_spoons()
 
-def _install_homebrew():
-    _log("Installing Homebrew", subheader=True)
+def install_homebrew():
+    log("Installing Homebrew", subheader=True)
     if find_executable("brew"):
-        _log("Hombrew exists, no need to install", level=1)
+        log("Hombrew exists, no need to install", level=1)
     else:
         if not ARGS.dry_run:
-            script_url = "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
-            os.system(f"/bin/bash -c \"$(curl -fsSL {script_url})\"")
+            script_url = CONFIGS["urls"]["homebrew_install"]
+            install_run = subprocess.run(
+                [
+                    "/bin/bash",
+                    "-c",
+                    f"$(curl -fsSL {script_url})",
+                ]
+            )
 
     if not ARGS.dry_run:
-        _log("Installing Homebrew bundle", level=1)
-        os.system("brew bundle install --global --no-upgrade --verbose --no-lock")
-
-def _install_hammerspoon_spoons():
-    _log("Installing Hammerspoon spoons", subheader=True)
-    base_url = "https://raw.githubusercontent.com/Hammerspoon/Spoons/master/Spoons"
-    spoons_path = Path("~/.hammerspoon/Spoons/").expanduser()
-    if not ARGS.dry_run:
-      spoons_path.mkdir(parents=True, exist_ok=True)
-    for spoon in SPOONS:
-        msg = f"Spoon {spoon}"
-        spoon_dir = f"{spoon}.spoon"
-        spoon_zip = f"{spoon_dir}.zip"
-        url = f"{base_url}/{spoon_zip}"
-        if (spoons_path / spoon_dir).exists():
-            msg += " (EXISTS!!)"
-        else:
-            if not ARGS.dry_run:
-                urlretrieve(url, str(spoons_path / spoon_zip))
-                shutil.unpack_archive(str(spoons_path / spoon_zip), str(spoons_path), "zip")
-                (spoons_path / spoon_zip).unlink()
-        _log(msg, level=1)
+        log("Installing Homebrew bundle", level=1)
+        subprocess.run(
+            [
+                "brew",
+                "bundle",
+                "install",
+                "--global",
+                "--no-upgrade",
+                "--verbose",
+                "--no-lock",
+            ]
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--link", action="store_true")
     parser.add_argument("--unlink", action="store_true")
+    parser.add_argument("--download-files", action="store_true")
+    parser.add_argument("--remove-files", action="store_true")
     parser.add_argument("--install", action="store_true")
     parser.add_argument("--dry-run", action="store_true")
 
